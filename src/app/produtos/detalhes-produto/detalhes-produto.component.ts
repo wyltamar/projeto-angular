cf52eedd5f6256a89a7d6d67a592109eec43017b
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-08-30 21:56:13 -0300

    Criando a página de detalhes de produtos

---
+++ src/app/produtos/detalhes-produto/detalhes-produto.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduto, produtos } from 'src/app/produtos';
+
+@Component({
+  selector: 'app-detalhes-produto',
+  templateUrl: './detalhes-produto.component.html',
+  styleUrls: ['./detalhes-produto.component.css']
+})
+export class DetalhesProdutoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

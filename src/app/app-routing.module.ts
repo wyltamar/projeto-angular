423b9407723a65472884bca6de3a9595312379ba
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-09-05 21:39:45 -0300

    Criando a página de carrinho

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.componen
 const routes: Routes = [
   { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
   {path: "", redirectTo: "produtos", pathMatch: "full"},
+  { path: 'carrinho', loadChildren: () => import('./carrinho/carrinho.module').then(m => m.CarrinhoModule) },
   {path:"**", component: NaoEncontradaComponent}
 ];
 

91f7de658d37a5aa9a6ca41a7f34d27c0e9b6bcd
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-09-09 19:41:23 -0300

    desenvolvendo a página de carrinho

--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CarrinhoService } from '../carrinho.service';
+import { IProdutoCarrinho } from '../produtos';
 
 @Component({
   selector: 'app-carrinho',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carrinho.component.css']
 })
 export class CarrinhoComponent implements OnInit {
+  itensCarrinho: IProdutoCarrinho[] = [];
+  total = 0;
 
-  constructor() { }
+  constructor(
+    public carrinhoService: CarrinhoService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.itensCarrinho = this.carrinhoService.obtemCarrinho();
+    this.calculaTotal();
+  }
+
+  calculaTotal(){
+    this.total = this.itensCarrinho.reduce((prev, curr)=> prev + (curr.preco * curr.quantidade), 0);
+  }
+
+  removerProdutoCarrinho(produtoId: number){
+    this.itensCarrinho = this.itensCarrinho.filter(item=> item.id !== produtoId)
+    this.carrinhoService.removerProdutoCarrinho(produtoId);
+    this.calculaTotal();
+  }
+
+  comprar(){
+    alert("Obrigado por comprar em nossa loja!")
+    this.carrinhoService.limparCarrinho();
+    this.router.navigate(["produtos"])
+
   }
 
 }

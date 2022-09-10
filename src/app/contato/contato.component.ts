744fa3631d6826b5e87a632d6dc1dfed40460cbb
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2022-09-10 07:47:44 -0300

    Criação da página de contatos

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contato',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contato.component.css']
 })
 export class ContatoComponent implements OnInit {
+  formContato = this.formBiulder.group({
+    nome: ["", [
+      Validators.minLength(4),
+      Validators.required
+    ]],
+    assunto: ["", [
+      Validators.minLength(10),
+      Validators.required
+    ]],
+    telefone: ["", [
+      Validators.minLength(11),
+      Validators.required
+    ]],
+    email: ["", [
+      Validators.email,
+      Validators.required
+    ]],
+    mensagem: ["", [
+      Validators.minLength(20),
+      Validators.required
+    ]]
+  });
 
-  constructor() { }
+  constructor(
+    private formBiulder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  enviarFormulario(){
+    alert("Sua mensagem foi enviada")
+    this.formContato.reset();
   }
 
 }
